4a3e3232663c0999495e9b011ad0756ffebfdb97
Richard Livsey <richard@livsey.org>
2009-06-12 12:36:57 +0200

    point spec helper to the right lib

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'asdf'
+require 'json_service'
 
 Spec::Runner.configure do |config|
   
