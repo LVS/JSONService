76ba2fdc840e909dd1492e390dde6596a3a5295f
John Cinnamond <jc@jandaweb.com>
2009-06-12 12:49:37 +0200

    Moved files around to reflect module hierarchy.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'json_service'
 
 Spec::Runner.configure do |config|
