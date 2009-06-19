718b340d1513d772edb520283f20eae83881e5c6
Andrew Jeffries <andy@andyjeffries.co.uk>
2009-06-19 10:50:19 +0100

    Added colour back in to debug calls, makes them easier to find in long development log listings

--- lib/lvs/json_service/logger.rb
+++ lib/lvs/json_service/logger.rb
@@ -3,6 +3,7 @@ module LVS
     module Logger
       def self.debug(message)
         if const_defined?(:RAILS_DEFAULT_LOGGER)
+          message = "  \033[1;4;32mLVS::JsonService\033[0m #{message}"
           RAILS_DEFAULT_LOGGER.debug(message)
         end
       end
