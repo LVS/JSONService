c75e3f826fcc7c4635e32702dc3129eeef67f992
Andrew Jeffries <andy@andyjeffries.co.uk>
2009-06-19 16:20:57 +0100

    Logger spec changed as it no longer uses RAILS_DEFAULT_LOGGER (or whatever the old way was) and now uses Rails.logger

--- lib/lvs/json_service/logger.rb
+++ lib/lvs/json_service/logger.rb
@@ -1,9 +1,12 @@
+require 'pp'
 module LVS
   module JsonService
     module Logger
       def self.debug(message)
-        message = "  \033[1;4;32mLVS::JsonService\033[0m #{message}"
-        Rails.logger.debug(message)
+        if defined?(Rails) && Rails.logger
+          message = "  \033[1;4;32mLVS::JsonService\033[0m #{message}"
+          Rails.logger.debug(message)
+        end
       end
     end
   end
--- spec/lvs/json_service/logger_spec.rb
+++ spec/lvs/json_service/logger_spec.rb
@@ -1,28 +1,42 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
+
 describe LVS::JsonService::Logger do
   before :each do
   end
 
   describe "debug" do
-    describe "with RAILS_DEFAULT_LOGGER set" do
+    describe "with Rails.logger set" do
       before :each do
         @mock_logger = mock()
-        LVS::JsonService::Logger.const_set(:RAILS_DEFAULT_LOGGER, @mock_logger)
+        
+        if !defined?(Rails)
+          class Rails
+            class << self
+              def logger=(value)
+                @logger = value
+              end
+              def logger
+                @logger
+              end
+            end
+          end
+        end
+        
+        Rails.logger = @mock_logger
       end
 
-      it "should pass the message to RAILS_DEFAULT_LOGGER" do
+      it "should pass the message to Rails.logger" do
         message = "Some debug message"
-        @mock_logger.should_receive(:debug).with(message)
+        coloured_message = "  \033[1;4;32mLVS::JsonService\033[0m #{message}"
+        @mock_logger.should_receive(:debug).with(coloured_message)
         LVS::JsonService::Logger.debug(message)
       end
     end
 
-    describe "without RAILS_ROOT set" do
+    describe "without Rails defined" do
       before :each do
-        LVS::JsonService::Logger.module_eval do
-          remove_const(:RAILS_DEFAULT_LOGGER) if const_defined?(:RAILS_DEFAULT_LOGGER)
-        end
+        Object.send(:remove_const, :Rails)
       end
 
       it "should not raise an exception" do
